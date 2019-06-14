8facd5413af6f211f34c283f056df4fe8f16e8ce
lovely <ldde07@gmail.com>
2019-06-14 16:04:54 -0700

    Add the about us dropdown

--- themes/rcforward/js/hamburger.js
+++ themes/rcforward/js/hamburger.js
@@ -1,9 +1,14 @@
 (function($){
 
     $(function(){
+        // hamburger menu
         $('#toggle-nav').on('click', function(){
             $('#primary-menu').toggle('is-active');
         });
+        // about us dropdown
+         $('#menu-item-71').on('click', function() {
+           $('.sub-menu').toggle('is-active');
+         });
     });// end doc ready
 
 })(jQuery);
