c3154b60076e9747f5000cf3eaa457cdf84db804
verasunyy <verasun@live.com>
2019-06-16 19:32:33 -0700

    add template for contact page

--- themes/rcforward/page-contact.php
+++ themes/rcforward/page-contact.php
@@ -9,17 +9,21 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+	</header><!-- .entry-header -->
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+	<div class="entry-content"> 
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
 
-			<?php endwhile; // End of the loop. ?>
+<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
+
 <?php get_footer(); ?>
