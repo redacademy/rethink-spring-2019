9ab71cb1919eb8b62cb7f4d2013a71197ad24341
lovely <ldde07@gmail.com>
2019-06-18 10:25:38 -0700

    Change style for header and add charity.scss

--- themes/rcforward/js/hamburger.js
+++ themes/rcforward/js/hamburger.js
@@ -2,18 +2,22 @@
   $(function() {
     // check if the toggle nav is visible e.g. mobile only
     if ($('.hamburger').css('display') !== 'none') {
-        console.log('is mobile');
       // hamburger menu
       $('#toggle-nav').on('click', function() {
-        $('#primary-menu').slideToggle('is-active');
+        $('#primary-menu').slideToggle();
       });
       // about us dropdown
       $('#menu-item-71').on('click', function() {
-        $('.sub-menu').slideToggle('is-active');
+        $('.sub-menu').slideToggle();
       });
     } // end if
-    else {
-        console.log('not mobile');
-    }
+   
+
+
+
+
+
+
+
   }); // end doc ready
 })(jQuery);
