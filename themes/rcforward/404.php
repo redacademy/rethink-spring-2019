cb2db039e2407cc60122c1fc5e241b912b64743e
lovely <ldde07@gmail.com>
2019-06-20 13:45:54 -0700

    Add 404 css and updated contact page

--- themes/rcforward/404.php
+++ themes/rcforward/404.php
@@ -9,12 +9,13 @@
 
 get_header(); ?>
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Hmmmm....' ); ?></h1>
-				</header><!-- .page-header -->
+				<div class="image-404">
+					<img> 
+				</div>
 
 				<div class="page-content">
+					<h1 class="page-title"><?php echo esc_html( 'Hmmmm....' ); ?></h1>
 					<p><?php echo esc_html( 'We cannot seem to find the page your looking for'); ?></p>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+				</div>
+			</section>
 <?php get_footer(); ?>
