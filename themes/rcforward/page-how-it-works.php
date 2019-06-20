5ee9777e1c60813d267a57b64c44d195c9b18144
Rosh <rosh_408@hotmail.com>
2019-06-20 10:59:30 -0700

    Style how it works entry header

--- themes/rcforward/page-how-it-works.php
+++ themes/rcforward/page-how-it-works.php
@@ -10,42 +10,46 @@
 get_header(); ?>
 
 
-<?php while ( have_posts() ) : the_post(); ?>
+<?php while (have_posts()) : the_post(); ?>
 	<header class="entry-header">
-		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+		<?php the_title('<h2 class="entry-title">', '</h2>'); ?>
 		<p class="page-description"><?php echo CFS()->get('how_it_works_page_description') ?></p>
 		<div class="how-it-works-steps">
-		<?php 
-		$work_steps = CFS()->get( 'how_it_works' );
-		foreach ( $work_steps as $work_step ) :
-		?>
-		<div class="work-step">
-		<img class="work-step-icon" src="<?php echo $work_step['icon']; ?>" alt="Step Icon"/>	
-		<span class="mobile-step-icon"><?php echo  $work_step['mobile_icon'];?></span>
-		<p class = "work-step-title"><?php echo $work_step['title']; ?></p>
-		<p class = "work-step-description"><?php echo $work_step['description']; ?></p>
-		</div>
-		<?php endforeach; ?>
-
-		<div class="entry-donate-page">
-		<p class="get-started">Get Started!</p>
-		<a class="donate-page-button" href="<?php echo home_url(); ?>/donate">Donate</a>
-		</div>
-		</div>
+			<!-- <div class="icon-steps">
+			</div> -->
+			<div class="text-steps">
+				<?php
+				$work_steps = CFS()->get('how_it_works');
+				foreach ($work_steps as $work_step) :
+					?>
+					<div class="work-step">
+						<img class="work-step-icon" src="<?php echo $work_step['icon']; ?>" alt="Step Icon" />
+						<img class="mobile-step" src="<?php echo $work_step['mobile_icon']; ?>" alt="Mobile Icon"/>
+						<div class="step-text">
+						<p class="work-step-title"><?php echo $work_step['title']; ?></p>
+						<p class="work-step-description"><?php echo $work_step['description']; ?></p>
+				</div>
+					</div>
+				<?php endforeach; ?>
+				<div class="entry-donate-page">
+					<p class="get-started">Get Started!</p>
+					<a class="donate-page-button" href="<?php echo home_url(); ?>/donate">Donate</a>
+				</div>
+			</div>
+		</div> <!-- how-it-works-steps -->
 	</header><!-- .entry-header -->
 	<div class="entry-contact-us">
-		<p>Contact us to discuss any other custom giving need or if you have questions?</p>
+		<p class="mobile-text">Have donations questions?</p>
+		<p class="desktop-text">Contact us to discuss any other custom giving need or if you have questions?</p>
 		<a class="contact-us-button" href="<?php echo home_url(); ?>/contact">Contact Us</a>
 	</div>
 	<div class="entry-main">
-
 		<?php the_content(); ?>
-
 	</div>
-	<?php get_template_part( 'template-parts/content', 'why-donate' ); ?>
-	<?php get_template_part( 'template-parts/content', 'charities' ); ?>
+	<?php get_template_part('template-parts/content', 'why-donate'); ?>
+	<?php get_template_part('template-parts/content', 'charities'); ?>
 	<?php get_template_part('template-parts/content', 'custom-plan'); ?>
-
-<?php endwhile; // End of the loop. ?>
+<?php endwhile;
+?>
 
 <?php get_footer(); ?>
