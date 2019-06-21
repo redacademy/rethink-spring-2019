d21f5fd0a80b43780e0dc552ce2f02e9cf4a7605
lovely <ldde07@gmail.com>
2019-06-21 01:27:18 -0700

    Style the blog page

--- themes/rcforward/404.php
+++ themes/rcforward/404.php
@@ -1,27 +1,29 @@
 <?php
 /**
  * The template for displaying 404 pages (not found).
+ * 
+ * Template Name: 404 Page
  *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ * 
  *
- * @package RED_Starter_Theme
+ * @package RC Forward
  */
-get_header(); ?>
+get_header();?>
 
-	<?php while (have_posts()): the_post();?>
-	
-	<section id="post-<?php the_ID();?>" <?php post_class();?>>
-		<div class="content-404">
-			<div class="background">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/output-onlinepngtools.png" alt="404 image">
-			</div>
-		
-			<div class="content">
-				<?php the_content();?>
-			</div>
-		</div>
-	</section>
+    <?php while (have_posts()): the_post();?>
 
-	<?php endwhile;?>
+	    <section class="container-404" id="post-<?php the_ID();?>" <?php post_class();?>>
+	       
+	            <div class="search-girl">
+		                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/output-onlinepngtools.png" alt="404 image">
+	            </div>
 
-<?php get_footer(); ?>
+	            <div class="error-text">
+	                <?php the_content();?>
+	            </div>
+	     
+	    </section>
+
+	    <?php endwhile;?>
+
+<?php get_footer();?>
