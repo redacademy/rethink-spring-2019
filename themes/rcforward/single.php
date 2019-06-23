865691964eabfe118817c4143d19a40670df3c3a
lovely <ldde07@gmail.com>
2019-06-23 04:57:18 -0700

    Style single-post and blog-post

--- themes/rcforward/single.php
+++ themes/rcforward/single.php
@@ -9,13 +9,13 @@ get_header(); ?>
 
 <?php while (have_posts()) : the_post(); ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 			<?php if (has_post_thumbnail()) : ?>
 				<?php the_post_thumbnail('large'); ?>
 			<?php endif; ?>
 		</header><!-- .entry-header -->
-		<main class="entry-content">
+		<article class="entry-content">
 			<div class="entry-title">
 				<p class="entry-meta"><?php red_starter_posted_on(); ?><p>
 				<?php the_title('<h2 class="entry-title">', '</h2>'); ?>
@@ -36,16 +36,16 @@ get_header(); ?>
 			<footer class="entry-footer">
 				<?php red_starter_entry_footer(); ?>
 			</footer><!-- .entry-footer -->
-		</main><!-- .entry-content -->
+			</article><!-- .entry-content -->
 		<div class="post-navigation">
-			<h3>More Articles</h3>
+			<h3 class="more-article">More Articles</h3>
 			<div class="more-post">
 				<?php
 					$prev_post = get_previous_post();
 					if (!empty($prev_post)) :
 						$prev__id = $prev_post->ID; ?>
 						<div class="previous-post">
-						<?php echo $prev_post->post_title ?>
+							<p class="pre-nav-1"><?php echo $prev_post->post_title ?></p>
 						<div class="prev-post-image">
 							<?php if (has_post_thumbnail($prev__id)) : ?>
 								<?php the_post_thumbnail($prev__id, 'large'); ?>
@@ -64,7 +64,11 @@ get_header(); ?>
 							</div><!-- .entry-main -->
 
 							</div>
-							<a class="read-more-button" href="<?php echo $prev_post->guid ?>">Read More <span class="read-more-arrow"> &#x203A; </span></a>
+
+							<div class="read-more">
+								<a class="read-more-button" href="<?php echo $prev_post->guid ?>">Read More </a>
+								<span class="read-more-arrow"> &#x203A; </span>
+							</div>
 						</div>
 						<?php endif ?>
 					
@@ -75,7 +79,7 @@ get_header(); ?>
 							$post_introduction = CFS()->get('post-introduction', $next_post_id);
 							$post_annoucement = CFS()->get('post-announcement', $next_post_id); ?>
 								<div class="next-post">
-							<?php echo $next_post->post_title ?>
+							<p class="next"> <?php echo $next_post->post_title ?></p>
 							<div class="next-post-image">
 								<?php if (has_post_thumbnail($next_post_id)) : ?>
 									<?php the_post_thumbnail($next_post_id, 'large'); ?>
@@ -98,8 +102,10 @@ get_header(); ?>
 								</div><!-- .entry-main -->
 						</div>
 
-								<a class="read-more-button" href="<?php echo $next_post->guid ?>">Read More <span class="read-more-arrow"> &#x203A; </span></a>
-
+								<div class="read-more">
+									<a class="read-more-button" href="<?php echo $next_post->guid ?>">Read More</a>
+										<span class="read-more-arrow"> &#x203A; </span>
+								</div>
 							</div>
 							<?php endif; ?>
 							
@@ -108,7 +114,7 @@ get_header(); ?>
 					</div>
 				</div>
 		
-	</article><!-- #post-## -->
+						</main><!-- #post-## -->
 
 
 
