b26b2eaa302270cba968e6c4f85c4c8a43b34d5e
lovely <ldde07@gmail.com>
2019-06-23 13:07:33 -0700

    Create blog.scss for blog page

--- themes/rcforward/404.php
+++ themes/rcforward/404.php
@@ -9,21 +9,14 @@
  * @package RC Forward
  */
 get_header();?>
-
     <?php while (have_posts()): the_post();?>
-
-	    <section class="container-404" id="post-<?php the_ID();?>" <?php post_class();?>>
-
-	            <div class="error-text">
-    				<?php the_content();?>
-				</div>
-	          		 
-				<div class="search-girl">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/output-onlinepngtools.png" alt="404 image">
-				</div>
-			
-	    </section>
-
-	    <?php endwhile;?>
-
+			    <section class="container-404" id="post-<?php the_ID();?>" <?php post_class();?>>
+						<div class="search-girl">
+							<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/output-onlinepngtools.png" alt="404 image"> -->
+						</div>
+			            <div class="error-text">
+		    				<?php the_content();?>
+						</div>
+			    </section>
+			    <?php endwhile;?>
 <?php get_footer();?>
