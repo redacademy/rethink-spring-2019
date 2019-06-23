590982f232a1574fd8ddaf179c3fcc0eaa7e0769
verasunyy <verasun@live.com>
2019-06-23 13:34:56 -0700

    change 404.php

--- themes/rcforward/404.php
+++ themes/rcforward/404.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying 404 pages (not found).
  * 
- * Template Name: 404 Page
+ * 
  *
  * 
  *
@@ -10,20 +10,19 @@
  */
 get_header();?>
 
-    <?php while (have_posts()): the_post();?>
 
-	    <section class="container-404" id="post-<?php the_ID();?>" <?php post_class();?>>
+	    <section class="container-404">
+	<div class="search-girl">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/output-onlinepngtools.png" alt="404 image">
+	</div>
 
 	            <div class="error-text">
-    				<?php the_content();?>
+    				<h2>Hmmm....</h2>
+					<p>We can't seem to find the page you're looking for</p>
 				</div>
 	          		 
-				<div class="search-girl">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/output-onlinepngtools.png" alt="404 image">
-				</div>
 			
 	    </section>
 
-	    <?php endwhile;?>
 
 <?php get_footer();?>
