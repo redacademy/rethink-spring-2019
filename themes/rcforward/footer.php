7caac6f5a608a8194fe44c724ebc4edf323cf0e3
Rosh <rosh_408@hotmail.com>
2019-06-25 09:21:14 -0700

    Fix width size on faq page // Create mailto href for contact button on faq page

--- themes/rcforward/footer.php
+++ themes/rcforward/footer.php
@@ -40,7 +40,7 @@
 				<p><?php echo CFS()->get('parent_charity_description', $front_page_id); ?></p>
 			</div>
 			<div class="font-link">
-				<a class="mail" href="<?php echo CFS()->get('parent_charity_email', $front_page_id); ?>"></a>
+				<a class="mail" href="mailto:<?php echo CFS()->get('parent_charity_email', $front_page_id); ?>"></a>
 				<a class="facebook" href="<?php echo CFS()->get('parent_charity_facebook_link', $front_page_id); ?>"></a>
 				<!-- <i class="far fa-envelope fa-2x"></i> -->
 				<!-- <i class="fab fa-facebook-f fa-2x"></i> -->
