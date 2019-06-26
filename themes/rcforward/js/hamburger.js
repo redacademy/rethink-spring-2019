5586b8f20a7d1eb984a9bf41b2097228374fb32d
verasunyy <verasun@live.com>
2019-06-26 15:20:15 -0700

    fix header hover issue

--- themes/rcforward/js/hamburger.js
+++ themes/rcforward/js/hamburger.js
@@ -1,19 +1,33 @@
 (function($) {
   $(function() {
-    // check if the toggle nav is visible e.g. mobile only
-    if ($('.hamburger').css('display') !== 'none') {
-      // hamburger menu
-      $('#toggle-nav').on('click', function() {
-        $('#site-navigation').slideToggle();
-      });
-      // about us dropdown
-      $('.menu-item-has-children').on('click', function() {
-        $(this).find('.sub-menu').slideToggle();
-        $(this).toggleClass('menu-active');
-      });
-      $('.sub-menu li').on('click',function(){
-        $('#site-navigation').slideToggle();
-      })
-    } // end if
+
+    if(window.innerWidth < 800){
+      console.log('is mobile');
+
+      // check if the toggle nav is visible e.g. mobile only
+      if ($('.hamburger').css('display') !== 'none') {
+        // hamburger menu
+        $('#toggle-nav').on('click', function() {
+          $('#site-navigation').slideToggle();
+        });
+        // about us dropdown
+        $('.menu-item-has-children').on('click', function() {
+          $(this).find('.sub-menu').slideToggle();
+          $(this).toggleClass('menu-active');
+        });
+        $('.sub-menu li').on('click',function(){
+          $('#site-navigation').slideToggle();
+        })
+
+        // $('#toggle-nav').click();
+
+      } // end if
+
+     
+    }
+    else {
+      console.log('not mobile');
+    }
+    
   }); // end doc ready
 })(jQuery);
